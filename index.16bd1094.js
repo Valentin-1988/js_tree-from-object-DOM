99e622ebf1cc1a44474cf622b956a87356c2f98e
Valentin_Kuianov <kuyanov1988@mail.com>
2024-08-19 19:14:56 +0300

    make build

---
+++ index.16bd1094.js
@@ -0,0 +1,2 @@
+!function e(n,t){if(0!==Object.keys(t).length){var r=document.createElement("ul");for(var a in n.appendChild(r),t)if(t.hasOwnProperty(a)){var i=document.createElement("li");i.textContent=a,r.appendChild(i),e(i,t[a])}}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.16bd1094.js.map
